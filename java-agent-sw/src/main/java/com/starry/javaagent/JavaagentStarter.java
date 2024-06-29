2419b977c548e0ecfb12b3eb74f01f532131130a
zhouzixin <zhouzixin@apache.org>
2024-06-29 22:30:53 +0800

    Add Java Demo

---
+++ java-agent-sw/src/main/java/com/starry/javaagent/JavaagentStarter.java
@@ -0,0 +1,11 @@
+package com.starry.javaagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaagentStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaagentStarter.class, args);
+    }
+}
